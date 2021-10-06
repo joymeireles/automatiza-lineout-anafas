792f6795dea924ee452e812c27e00ee8b5eee6d5
Joyce <joyce.meirelesiq@gmail.com>
2021-10-05 23:09:53 -0300

    Apaga dados anteriores e add kA

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from os.path import join
 import matplotlib.pyplot as plt
@@ -83,24 +84,67 @@ def filtra_planilha(df: pd.DataFrame,
             bdf = pd.concat([bdf, ibdf])
     return bdf
 
+def atribui_curto(df_lineout: pd.DataFrame,
+                  line_outs: List[DadosDisjuntor],
+                  curto_barras: List[DadosBarra]) -> pd.DataFrame:
+    df_lineout.loc[:, "Monofásico (kA)"] = np.nan
+    df_lineout.loc[:, "Trifásico (kA)"] = np.nan
+    df_lineout.loc[:, "Bifásico-Terra (kA)"] = np.nan
+    df_lineout.loc[:, "Monofásico (%)"] = np.nan
+    df_lineout.loc[:, "Trifásico (%)"] = np.nan
+    df_lineout.loc[:, "Bifásico-Terra (%)"] = np.nan
+    df_lineout.loc[:, "Situação"] = np.nan
+
+    for line_out in line_outs:
+        filtro_de = (df_lineout["Barra DE"] == line_out.barra_de)
+        filtro_para = (df_lineout["Barra PARA"] == line_out.barra_para)
+        df_lineout.loc[filtro_de & filtro_para, "Monofásico (kA)"] = line_out.curto_mono
+        df_lineout.loc[filtro_de & filtro_para, "Trifásico (kA)"] = line_out.curto_tri
+        df_lineout.loc[filtro_de & filtro_para, "Bifásico-Terra (kA)"] = line_out.curto_bif
+
+    for curto_barra in curto_barras:
+        filtro_de = (df_lineout["Barra DE"] == curto_barra.barra_de)
+        df_lineout.loc[filtro_de & (df_lineout["Barra PARA"] == np.nan), "Monofásico (kA)"] = curto_barra.curto_mono
+        df_lineout.loc[filtro_de & (df_lineout["Barra PARA"] == np.nan), "Trifásico (kA)"] = curto_barra.curto_tri
+        df_lineout.loc[filtro_de & (df_lineout["Barra PARA"] == np.nan), "Bifásico-Terra (kA)"] = curto_barra.curto_bif
+
+        # if ((df["Barra De"] == line_out.barra_de) and
+        #     (df["Barra Para"] == line_out.barra_para)):
+        #     df["Monofásico (kA)"] = line_out.curto_mono
+        #     df["Trifásico (kA)"] = line_out.curto_tri
+        #     df["Bifásico-Terra (kA)"] = line_out.curto_bif
+            # if ((df["Capacidade de interrupção simétrica (kA)"] is not str) and
+            #     (df["Capacidade de interrupção simétrica (kA)"] is not None)):
+            #     df["Monofásico (%)"] = 100 * (line_out.curto_mono / 
+            #                                   df["Capacidade de interrupção simétrica (kA)"])
+            #     df["Trifásico (%)"] = 100 * (line_out.curto_tri / 
+            #                                   df["Capacidade de interrupção simétrica (kA)"])
+            #     df["Bifásico (%)"] = 100 * (line_out.curto_bif / 
+            #                                   df["Capacidade de interrupção simétrica (kA)"])
+    return df_lineout
+
 
 if __name__ == "__main__":
 
     dir_base = "C:\\Users\\Joyce ONS\\Desktop\\Joyce ONS\\5- EGP\\Curto-Circuito"
-    caminho_planilha = join(dir_base, "_automatizar processos\\_entrega final\\_META_LINE OUT 2021_R1.xlsx") # noqa
+    caminho_planilha = join(dir_base, "_automatizar processos\\_entrega final\\_META_LINE OUT 2026_R1.xlsx") # noqa
     caminho_relatorio = join(dir_base,"_automatiza ANAFAS\\relatorio anafas tres barras.rel")
-    caminho_planilha_saida = ""
+    caminho_planilha_saida = "_automatiza ANAFAS\\teste_saída.xlsx"
     line_outs, curto_barras = ler_relatorio_anafas(caminho_relatorio)
     df = pd.read_excel(caminho_planilha, "Dados Disjuntores", header=1)
 
     # Faz operações
     df_lineout = filtra_planilha(df, curto_barras)
-    print(df_lineout)
+    df_saida = atribui_curto(df_lineout, line_outs, curto_barras)
+    
+    print(df_lineout["Monofásico (kA)"])
+    print(df_lineout["Barra DE"])
+  
 
 
 
     # Salva planilha
-    # df.to_excel(join(dir_base, caminho_planilha_saida))
+    df_saida.to_excel(join(dir_base, caminho_planilha_saida))
 
     # df.columns
     # disj_1257 = df.loc[(df["Barra DE"] == 1257) & (df["Barra PARA"] == 1219), "Monofásico (%)"]
